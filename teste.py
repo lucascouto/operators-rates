8d0f0944592c5c3be1aab0fbb2dde4d22b2cded0
Lucas Couto <loc.unb@gmail.com>
2019-07-08 18:04:38 -0300

    bug fix for prefix search

--- teste.py
+++ teste.py
@@ -22,18 +22,25 @@ for i in range(num_operators):
 
 print(all_operators)
 
+
 phone_number = int(input('Enter the phone number to search the lowest rate: '))
 phone_number_digits = [int(digit) for digit in str(phone_number)]
 
+
+
 #FIND THE LOWEST RATE
 lowest_rate = sys.maxsize
+
 for operator in all_operators:
+
+    max_prefix_length = 0
+    current_prefix = 0
+
     for prefix in list(operator[1].keys()):
         
         prefix_digits = [int(digit) for digit in str(prefix)]
-        max_prefix_length = 0
         is_valid_prefix = True
-        current_prefix = 0
+        
 
         for i in range(len(prefix_digits)):
             if prefix_digits[i] != phone_number_digits[i]:
@@ -44,13 +51,12 @@ for operator in all_operators:
             max_prefix_length = len(prefix_digits)
             current_prefix = prefix
     
-    print(f'Operadora: ${operator[0]}')
+    print(f'Operadora: {operator[0]}')
     if current_prefix != 0:
         print(f'O prefixo: {current_prefix}')
-        print(f'O valor: ${operator[1][prefix]}')
+        print(f'O valor: ${operator[1][current_prefix]}')
     else:
         print('Sem prefixo correspondente!')
-            
 
 
 
